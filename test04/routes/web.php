ca6e2be3c2c51f1b2f8d4a5a5a0355defe4d522c
Stefany2 <1459565@senati.pe>
2024-04-18 12:37:10 -0500

    login 01

---
+++ test04/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
