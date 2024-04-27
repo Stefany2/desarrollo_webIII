eb7baf4db714072d4398b4343b497d5ab593af4f
Stefany2 <1459565@senati.pe>
2024-04-27 18:17:32 -0500

    Menu y inicio de mi proyecto

---
+++ tienda1/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Ruta de Inicio
+Route::get('/inicio', function () {
+    return view('inicio');
+})->name('inicio');
+
+// Ruta de contactanos
+Route::get('/contactanos', function () {
+    return view('contactanos');
+})->name('contactanos');
